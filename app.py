865207d60b9414fe72a6470b31e3797c05437997
imdebamrita <imdebamrita@gmail.com>
2025-07-23 15:59:38 +0530

    fix a bug

--- app.py
+++ app.py
@@ -85,7 +85,8 @@ def user_input(user_question):
             return
 
         # Load FAISS index without the allow_dangerous_deserialization parameter
-        new_db = FAISS.load_local("faiss_index", embeddings)
+        new_db = FAISS.load_local(
+            "faiss_index", embeddings, allow_dangerous_deserialization=True)
         docs = new_db.similarity_search(user_question)
 
         chain = get_conversational_chain()
